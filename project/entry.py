ced133222b92d02b616d5a1285e5e2ffe9220dad
root <luowensheng2018@gmail.com>
2021-07-27 03:57:12 +0900

    add RequestHandler and Results classes

--- project/entry.py
+++ project/entry.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 import cv2
 import urllib
 from .model import KRCNN
+from .handler import RequestHandler
 import time
 from . import utils 
 
@@ -15,88 +16,52 @@ from . import utils
 bp = Blueprint('project', __name__)
 model = KRCNN()
 
-DIR = os.path.join(os.getcwd(), "project")
-
-ALLOWED_EXTENSIONS  =   ['jpeg','gif','png','jpg']
 
 
 @bp.route("/")
 def home():   
-    return  render_template("home.html", contains_prediction=False)
+    return render_template("home.html", contains_prediction=False)
 
 
 
-@bp.route("/pred", methods=["GET","POST"])
-def pred():
+@bp.route("/pred", methods=["POST"])
+def pred()->str:
 
-    form = request.args.to_dict()
-    if len(form)==0:
-        form = request.form.to_dict()
+    request_handler = RequestHandler(request)
+    request_result = request_handler.process_request()
     
-    from_url = 'url' in form
+    if request_result.failed:
+        flash(request_result.message)
+        return render_template("home.html", contains_prediction=False)
 
-    if from_url :   
-        filename = f"out_{time.time()}.png" 
-        path =  form['url']   
-        req = urllib.request.urlopen(path)
-        arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        img = cv2.imdecode(arr, -1)
-        path = os.path.join(DIR, "static", "outputs", filename)
-        img_saved_successfully = cv2.imwrite(path, img)
-
-        if not img_saved_successfully:
-            flash("Image has not been loaded succesfully. There might be a problem with the url!")
-            return  render_template("home.html", contains_prediction=False)
-                    
-    else:
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        ext = filename.split(".")[-1].lower()
-        extension_is_allowed = ext in ALLOWED_EXTENSIONS
-
-        if not extension_is_allowed:
-            flash(f"Extension [{ext}] is not allowed!\nHere is a list of allowed extenssions:\n\t{ALLOWED_EXTENSIONS}")
-            return  render_template("home.html", contains_prediction=False)
-        
-        fpath = os.path.join(DIR, "static", "outputs", filename) 
-        file.save(fpath)  
-        img = cv2.imread(fpath)
-    
-    img_not_read = img is None
-
-    if img_not_read:
-       flash("Image not loaded correctly. Please try again.")
-       return  render_template("home.html", contains_prediction=False) 
-
-    path = os.path.join("static", "outputs", filename) 
+    path = os.path.join("static", "outputs", request_handler.filename) 
 
     out_paths = []
-    make_prediction(img, 
+    make_prediction(request_handler, 
                     out_paths,
-                    filename=f"out_pred_{filename}", 
                     patchsize=500, 
                     do_patch_prediction=False
                     )
     n=len(out_paths)
-    flash("Image has been processed succesfully! Here are the results:\n")
+    flash("The image has been processed succesfully! Here are the results:\n")
     return  render_template("home.html", contains_prediction=True, path=path, out_paths=out_paths, n=n)
 
 
 
-def make_prediction(img: np.array, out_paths: list, filename:str, patchsize:int, do_patch_prediction:bool=False) -> None:
+def make_prediction(request_handler: RequestHandler, out_paths: list, patchsize:int, do_patch_prediction:bool=False) -> None:
 
     if do_patch_prediction:
        output_img = utils.run_on_patches(model,
-                                         img, 
+                                         request_handler.img, 
                                          patchsize=patchsize,
                                          add_separation_lines = True, 
                                          width = 5
                                          )
     else:
-       output_img = model(img)
+       output_img = model(request_handler.img)
     
-    out_path = os.path.join("static", "outputs", filename)
-    cv2.imwrite(os.path.join(DIR, out_path), output_img )
+    out_path = os.path.join("static", "outputs", request_handler.output_filename)
+    cv2.imwrite(os.path.join(request_handler.DIR, out_path), output_img )
     out_paths.append(out_path)
 
        
--- project/handler.py
+++ project/handler.py
@@ -3,13 +3,29 @@ from werkzeug.utils import secure_filename
 import os
 import cv2
 import urllib
+import time
+import numpy as np
 
 
-class Handler:
-    def __init__(request):
+class Results:
+    def __init__(self, message: str, failed:str):
+        self.message = message
+        self.failed = failed
+
+
+class RequestHandler:
+
+    ALLOWED_EXTENSIONS = ['jpeg','gif','png','jpg']
+
+    def __init__(self,request):
+
         self.request = request
         self.form = self.request_to_dict(request)
-        from_url = 'url' in form
+        self.request_is_from_url = 'url' in self.form
+        self.filename = None
+        self.img = None
+        self.path = None
+        self.DIR = os.path.join(os.getcwd(), "project")
 
     def request_to_dict(self, request):
         form = self.request.args.to_dict()
@@ -17,43 +33,70 @@ class Handler:
             form = request.form.to_dict()
         return form
         
-        
+    def process_request(self):
+        if self.request_is_from_url:
+           return self._process_request_from_url()
+        return self._process_request_from_upload()        
 
-    def process_request_from_url(self):     
-        filename = f"out_{time.time()}.png" 
-        path =  form['url']   
-        req = urllib.request.urlopen(path)
+    def _process_request_from_url(self)-> Results:     
+
+        self.filename = f"out_{time.time()}.jpg" 
+        path =  self.form['url'] 
+        try: 
+          req = urllib.request.urlopen(path)
+        except Exception as message:
+           failed = True  
+           return Results(message=message, failed=failed) 
+
         arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        img = cv2.imdecode(arr, -1)
-        path = os.path.join(DIR, "static", "outputs", filename)
-        img_saved_successfully = cv2.imwrite(path, img)
-        return img, path, img_saved_successfully
+        self.img = cv2.imdecode(arr, -1)
+        self.path = os.path.join(self.DIR, "static", "outputs", self.filename)
+        img_saved_successfully = cv2.imwrite(path, self.img)
 
+        if not img_saved_successfully:
+           message =  "Image has not been loaded succesfully. There might be a problem with the url!"  
+           failed = True         
+           
+        else:
+            message = None
+            failed = False
 
-    def process_request_from_upload(self):     
+        return Results(message=message, failed=failed)    
+
+    def _process_request_from_upload(self)-> Results:  
 
         file = self.request.files['file']
-        filename = secure_filename(file.filename)
-        ext = filename.split(".")[-1].lower()
-        #extension_is_allowed = ext in ALLOWED_EXTENSIONS
+        self.filename = secure_filename(file.filename)
+        ext = self.filename.split(".")[-1].lower()
+        extension_is_allowed = ext in self.ALLOWED_EXTENSIONS
+
+        if not extension_is_allowed:
+            message =  f"Extension [{ext}] is not allowed!\nHere is a list of allowed extenssions:\n\t{ALLOWED_EXTENSIONS}"
+            failed = True  
+        else:         
         
-        path = os.path.join(DIR, "static", "outputs", filename) 
-        file.save(path)  
-        img = cv2.imread(path)
-    
-        img_saved_successfully = not img is None
-        return img, path, img_saved_successfully
+            self.path = os.path.join(self.DIR, "static", "outputs", self.filename) 
+            file.save(self.path)  
+            self.img = cv2.imread(self.path)
+        
+            img_not_read =  self.img is None
+
+            if img_not_read:
+               message =  f"Extension [{ext}] is not allowed!\nHere is a list of allowed extenssions:\n\t{ALLOWED_EXTENSIONS}"
+               failed = True 
+
+            else:
+               message = None
+               failed = False     
+
+        return Results(message=message, failed=failed)
+
+    @property
+    def output_filename(self):
+
+        if self.filename is None:
+           return None
+
+        return f"out_pred_{self.filename}"     
 
 
-    path = os.path.join("static", "outputs", filename) 
-
-    out_paths = []
-    make_prediction(img, 
-                    out_paths,
-                    filename=f"out_pred_{filename}", 
-                    patchsize=500, 
-                    do_patch_prediction=False
-                    )
-    n=len(out_paths)
-    flash("Image has been processed succesfully! Here are the results:\n")
-    return  render_template("home.html", contains_prediction=True, path=path, out_paths=out_paths, n=n)
